6232fd836bd0c4ea2c4c754e313e78d820f3a701
Đorđe Milanović <djordje200179@gmail.com>
2021-06-20 23:07:56 +0200

    Minor reformatting

--- Test/transformations_test.cpp
+++ Test/transformations_test.cpp
@@ -6,10 +6,10 @@ void transformations_test() {
 	std::cout << "TEST: Transformations" << std::endl;
 
 	ImageManipulation::Image image("res/image.bmp");
-	
+
 	image.flip(ImageManipulation::FlipDirection::HORIZONTAL);
 	image.flip(ImageManipulation::FlipDirection::VERTICAL);
 	//rotate_image(image, RotationDirection::LEFT);
-	
+
 	image.save_image("res/image_test1.bmp");
 }
